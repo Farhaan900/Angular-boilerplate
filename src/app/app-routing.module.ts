f8d214cf05f4d0d496c934ab64f0caf1998807d1
Farhaan Shaikh <farhaan.shaikh@pure-systems.com>
2019-11-25 18:05:49 +0100

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BodyComponent } from './body/body.component';
+import { Body2Component } from './body2/body2.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  { path: 'body', component: BodyComponent },
+  { path: 'body2', component: Body2Component},
+  { path: '**', redirectTo: 'body' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
